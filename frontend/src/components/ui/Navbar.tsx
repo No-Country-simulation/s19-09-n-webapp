e363e3668eb624ad45da5817bb8ab0d24060aac3
Luis Delgado <dev.ledg555@gmail.com>
2024-11-30 11:36:50 -0600

    fix: replace hrefs by "link to" and "redirect"

--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -1,6 +1,8 @@
-import { AppBar, Box, Button, Container, Link, Toolbar } from '@mui/material'
+import { AppBar, Box, Button, Container, Toolbar } from '@mui/material'
+import {Link} from "react-router-dom"
 import { styled } from '@mui/material/styles'
 import UserButton from './UserButton'
+import {redirect} from "react-router-dom"
 
 // Styled components
 const StyledAppBar = styled(AppBar)({
@@ -19,17 +21,23 @@ const NavLink = styled(Link)({
   },
 })
 
+const HomeButton = styled(Link)({
+  fontWeight: "bold",
+  fontSize: "24px",
+  color: "#604CC3",
+});
+
 
 export default function Navbar() {
   return (
     <StyledAppBar position="static">
-      <Container maxWidth="lg" >
-        <Toolbar sx={{ justifyContent: 'space-between', padding: '8px 0' }}>
+      <Container maxWidth="lg">
+        <Toolbar sx={{ justifyContent: "space-between", padding: "21px 0" }}>
           {/* Logo section */}
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Button variant="text" href="/" sx={{ fontWeight: 'bold', fontSize: '24px',color: '#604CC3' }}>
-              Roomiefind
-            </Button>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <HomeButton to="/" sx={{ textDecoration: "none" }}>
+              ROOMIEFIND{" "}
+            </HomeButton>
             {/* <img 
               src="/placeholder.svg?height=32&width=100" 
               alt="Mercado Libre"
@@ -38,19 +46,17 @@ export default function Navbar() {
           </Box>
 
           {/* Navigation section */}
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Button variant="outlined" href="properties" sx={{ ml: 4 }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Button variant="outlined" onClick={() => redirect("/")} sx={{ ml: 4 }}>
               Ver propiedades
-              </Button>
-            <NavLink href="login">Publicar mi propiedad</NavLink>
-            <NavLink href="#">Ayuda</NavLink>
+            </Button>
+            <NavLink to="login">Publicar mi propiedad</NavLink>
+            <NavLink to="#">Ayuda</NavLink>
             <UserButton></UserButton>
-            
-            
           </Box>
         </Toolbar>
       </Container>
     </StyledAppBar>
-  )
+  );
 }
 
