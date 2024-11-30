5f5167a5a7169f29108e1bd2f8b0f59eb0987183
Mesach Venegas <mesach.venegas@hotmail.com>
2024-11-29 18:50:37 -0800

    add: types base to real estate module

---
+++ backend/src/real-estate/types/real-estate.type.ts
@@ -0,0 +1,13 @@
+
+// FIXME: Conver to DTO model
+export type PropertyPhoto = {
+  id: string;
+  photo_url: string;
+}
+
+// FIXME: Conver to DTO model
+export type PropertyOwner = {
+  name: string;
+  email: string;
+  phone: string;
+}
