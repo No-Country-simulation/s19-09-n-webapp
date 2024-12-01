577cea93b5373f285a52220798f81b5a75bd165a
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-01 11:22:25 -0800

    changes: add ApiProperty to add swagger docs

--- backend/src/real-estate/dto/filter-real-sate.dto.ts
+++ backend/src/real-estate/dto/filter-real-sate.dto.ts
@@ -1,52 +1,63 @@
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 import { PropertyType, RentalPeriod} from '@prisma/client';
 import { IsOptional, IsEnum, IsBoolean, IsString, IsInt, Min, IsNumber } from 'class-validator';
 
 
 export class FilterRealEstateDto {
+  @ApiProperty({ example: 'New York' })
   @IsOptional()
   @IsString()
   city?: string;
 
+  @ApiProperty({ example: 'APARTMENT' })
   @IsOptional()
   @IsEnum(PropertyType)
   property_type?: PropertyType;
 
+  @ApiProperty({ example: 2 })
   @IsOptional()
   @IsInt()
   @Type(() => Number)
   max_occupants?: number;
 
+  @ApiProperty({ example: 145.50 })
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
   minPrice?: number;
 
+  @ApiProperty({ example: 215 })
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
   maxPrice?: number;
 
+  @ApiProperty({ example: 'WEEKLY' })
   @IsOptional()
   @IsEnum(RentalPeriod)
   rentalPeriod?: RentalPeriod;
 
+  @ApiProperty({ example: true })
   @IsOptional()
   @IsBoolean()
   @Type(() => Boolean)
   isFurnished?: boolean;
 
+  @ApiProperty({ example: true })
   @IsOptional()
   @IsBoolean()
   @Type(() => Boolean)
   isServicesIncluded?: boolean;
 
+  @ApiProperty({ example: 2 })
   @IsOptional()
   @IsInt()
   @Type(() => Number)
   @Min(1)
   page: number = 1;
 
+  @ApiProperty({ example: 10 })
   @IsOptional()
   @IsInt()
   @Type(() => Number)
