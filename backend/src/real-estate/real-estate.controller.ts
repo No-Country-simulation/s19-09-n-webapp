05b8ff4a5bfdaa33d407e2a65bbcff666ab748b9
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-01 11:14:29 -0800

    changes: add validation pipes y change controller to handle filter options on get Properties

--- backend/src/real-estate/real-estate.controller.ts
+++ backend/src/real-estate/real-estate.controller.ts
@@ -1,18 +1,20 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Req, Res } from '@nestjs/common';
+import {
+  Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res, UsePipes, ValidationPipe
+} from '@nestjs/common';
 import { Response } from 'express';
 
 import { RealEstateService } from './real-estate.service'
-import { CreateRealEstateDto } from './dto/create-real-estate.dto';
+import { FilterRealEstateDto } from './dto/filter-real-sate.dto';
+import { CreateRealEstateDto, UpdateRealEstateDto } from './dto/create-real-estate.dto';
 
 @Controller('real-estate')
 export class RealEstateController {
-  constructor(private readonly realEstateService: RealEstateService) {}
+  constructor(private readonly realEstateService: RealEstateService) { }
 
-  // REVIEW: Function and test to get all properties with pagination.
   @Get()
   @HttpCode(HttpStatus.OK)
-  async getAllProperties(page: number, limit: number) {
-    return this.realEstateService.getAllRealEstates(page, limit);
+  async getAllProperties(@Query() filters: FilterRealEstateDto) {
+    return this.realEstateService.getAllRealEstates(filters);
   }
 
   // TODO: Service to get property by ID
@@ -25,14 +27,16 @@ export class RealEstateController {
 
   // TODO: Service to create a new entity of Property
   @Post('/add')
+  @UsePipes(new ValidationPipe())
   createProperty(@Body() dto: CreateRealEstateDto) {
     return dto;
   }
 
   // TODO: Service to update a property
-  @Put('/update/:id')
-  updateProperty(@Param('id') id: string, @Req() req: Request, @Res() res: Response) {
-    res.status(HttpStatus.OK).json({});
+  @Patch('/update/:id')
+  @UsePipes(new ValidationPipe())
+  updateProperty(@Param('id') id: string, @Body() dto: UpdateRealEstateDto) {
+    return dto;
   }
 
   // TODO: Service to delete a property
