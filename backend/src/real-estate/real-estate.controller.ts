56afae42641a365bf802f00eaf68d97e4f898fce
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-02 14:10:49 -0800

    add: api-doc response

--- backend/src/real-estate/real-estate.controller.ts
+++ backend/src/real-estate/real-estate.controller.ts
@@ -1,10 +1,8 @@
-import {
-  Body, Controller, Delete, Get, Param, Patch, Post, Query, Res, UsePipes, ValidationPipe
-} from '@nestjs/common';
-import { Response } from 'express';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiAcceptedResponse, ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiResponse } from '@nestjs/swagger';
 
 import { RealEstateService } from './real-estate.service'
-import { CreateRealEstateDto, UpdateRealEstateDto } from './dto/create-real-estate.dto';
+import { CreateRealEstateDto, UpdateRealEstateDto } from './dto';
 import { FilterRealEstateByUserIdDto, FilterRealEstateDto } from './dto/filter-real-sate.dto';
 
 @Controller('real-estate')
@@ -12,38 +10,48 @@ export class RealEstateController {
   constructor(private readonly realEstateService: RealEstateService) { }
 
   @Get()
+  @ApiResponse({ status: 200, description: 'Success', type: [FilterRealEstateDto] })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   async getAllProperties(@Query() filters: FilterRealEstateDto) {
     return this.realEstateService.getAllRealEstates(filters);
   }
 
 
-  @Get('/my-properties/:id')
+  @Get('/my-properties')
+  @ApiResponse({ status: 200, description: 'Success', type: [FilterRealEstateDto] })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   async getMyProperties(@Query() id: FilterRealEstateByUserIdDto) {
     return this.realEstateService.GetPropertiesByUserID(id);
   }
 
   @Get(':id')
+  @ApiNotFoundResponse({ description: 'Property not found or not exists' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   getProperty(@Param('id') id: string) {
     return this.realEstateService.GetRealEstateById(id);
   }
 
   // TODO: Service to create a new entity of Property
   @Post('/add')
-  @UsePipes(new ValidationPipe())
+  @ApiCreatedResponse({ description: 'Property created successfully' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   createProperty(@Body() dto: CreateRealEstateDto) {
     return dto;
   }
 
   // TODO: Service to update a property
   @Patch('/update/:id')
-  @UsePipes(new ValidationPipe())
+  @ApiAcceptedResponse({ description: 'Property updated successfully' })
+  @ApiBadRequestResponse({ description: 'Bad request' })
   updateProperty(@Param('id') id: string, @Body() dto: UpdateRealEstateDto) {
     return dto;
   }
 
 
   @Delete('/delete/:id')
-  deleteProperty(@Param('id') id: string, @Res() res: Response) {
+  @ApiResponse({ status: 200, description: 'Deleted successfully' })
+  @ApiNotFoundResponse({ description: 'Property not found or not exists' })
+  deleteProperty(@Param('id') id: string) {
     return this.realEstateService.deleteRealEstate(id);
   }
 }
