371f277257a0286fdd59152d6cf7bb8e3a6d1915
RassielMex <rhcp94.rm@gmail.com>
2024-12-05 19:24:38 -0600

    feat(back): deleted routes in photos

--- backend/src/photos/photos.controller.ts
+++ backend/src/photos/photos.controller.ts
@@ -1,44 +1,44 @@
 import {
   Controller,
-  Get,
-  Post,
-  Param,
-  Delete,
-  UseInterceptors,
-  Body,
-  UploadedFile,
+  // Get,
+  // Post,
+  // Param,
+  // Delete,
+  // UseInterceptors,
+  // Body,
+  // UploadedFile,
 } from '@nestjs/common';
 import { PhotosService } from './photos.service';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { CreatePhotoDtoWithoutFile } from './dto/create-photo.dto';
+//import { FileInterceptor } from '@nestjs/platform-express';
+//import { CreatePhotoDtoWithoutFile } from './dto/create-photo.dto';
 
 @Controller('photos')
 export class PhotosController {
   constructor(private readonly photosService: PhotosService) {}
 
-  @Post()
-  @UseInterceptors(FileInterceptor('photo'))
-  create(
-    @Body() createPhoto: CreatePhotoDtoWithoutFile,
-    @UploadedFile() photo: Express.Multer.File,
-  ) {
-    return this.photosService.create({
-      property_id: createPhoto.property_id,
-      photo: photo,
-    });
-  }
-  @Get('/property/:propertyId')
-  findAll(@Param('propertyId') propertyId: string) {
-    return this.photosService.findAllByProperty(propertyId);
-  }
+  // @Post()
+  // @UseInterceptors(FileInterceptor('photo'))
+  // create(
+  //   @Body() createPhoto: CreatePhotoDtoWithoutFile,
+  //   @UploadedFile() photo: Express.Multer.File,
+  // ) {
+  //   return this.photosService.create({
+  //     property_id: createPhoto.property_id,
+  //     photo: photo,
+  //   });
+  // }
+  // @Get('/property/:propertyId')
+  // findAll(@Param('propertyId') propertyId: string) {
+  //   return this.photosService.findAllByProperty(propertyId);
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.photosService.findOne(id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.photosService.findOne(id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.photosService.remove(id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.photosService.remove(id);
+  // }
 }
