0c56f9c96f1309165b2667eb17253bfe04bfe1d0
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-07 09:21:38 -0800

    add: swagger doc

--- backend/src/photos/dto/create-photo.dto.ts
+++ backend/src/photos/dto/create-photo.dto.ts
@@ -1,10 +1,14 @@
 import { IsString, Validate } from 'class-validator';
 import { FileSize, IsImgType } from '../decorators/file-validators';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePhotoDto {
+  @ApiProperty({ type: File })
   @Validate(IsImgType)
   @Validate(FileSize, [2])
   photo: Express.Multer.File;
+
+  @ApiProperty()
   @IsString()
   property_id: string;
 }
