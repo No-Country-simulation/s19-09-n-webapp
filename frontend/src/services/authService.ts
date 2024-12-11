f95476c3b1dc65f5da6dabb6c6c5f69108f4622f
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-12-11 10:36:06 -0500

    chore(front): update authService

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -4,7 +4,10 @@ const baseUrl = "https://s19-09-n-back.vercel.app/api/v1/auth"
 
 export async function signup(registrationFields: object) {
   const res = await fetch(baseUrl+ "/register", buildAuthReq(registrationFields));
-  const data = res.json();
+  const data = await res.json();
+  if(data.statusCode == 400){
+    throw new Error(data.message);
+  }
   return data;
 }
 
