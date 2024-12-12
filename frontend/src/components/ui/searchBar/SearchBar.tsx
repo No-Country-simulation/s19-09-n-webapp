ca96ea0b6a973c88a3098b8c6e500a9ee6ea2558
Mesach Venegas <mesach.venegas@hotmail.com>
2024-12-11 16:04:52 -0800

    changes: responsive search bar

--- frontend/src/components/ui/searchBar/SearchBar.tsx
+++ frontend/src/components/ui/searchBar/SearchBar.tsx
@@ -14,117 +14,128 @@ import { Button } from "@mui/material";
 /* import Button from "@mui/material"; */
 
 function SearchBar() {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  return (
-    <Box
-      component="form"
-      sx={{
-        position: "relative",
-        display: "flex",
-        margin: "4rem 2rem 2rem 2rem",
-        padding: "1rem 1rem",
-        border: "2px",
-        borderStyle: "solid",
-        borderColor: theme.palette.primary.main,
-        borderRadius: "8px",
-        gap: "8px",
-      }}
-    >
-      <FormControl fullWidth>
-        <InputLabel id="alojamiento">Tipo de alojamiento</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="alojamiento"
-          /*     value={}
+	return (
+		<form action="" >
+			<Box
+				component="form"
+				sx={{
+					position: "relative",
+					display: "flex",
+					flexDirection: { xs: "column", md: "row" },
+					margin: "4rem 2rem 2rem 2rem",
+					padding: "1rem 1rem",
+					border: "2px",
+					borderStyle: "solid",
+					borderColor: theme.palette.primary.main,
+					borderRadius: "8px",
+					gap: "8px",
+				}}>
+				<FormControl fullWidth>
+					<InputLabel id="alojamiento">Tipo de alojamiento</InputLabel>
+					<Select
+						labelId="alojamiento"
+						id="alojamiento"
+						/*     value={}
             label={}
             onChange={} */
-        >
-          <MenuItem value={1}>
-            <BedIcon
-              sx={{
-                fontSize: "medium",
-                color: theme.palette.primary.main,
-                paddingRight: "4px",
-              }}
-            />
-            Habitación
-          </MenuItem>
-          <MenuItem value={2}>
-            <ApartmentIcon
-              sx={{
-                fontSize: "medium",
-                color: theme.palette.primary.main,
-                paddingRight: "4px",
-              }}
-            />
-            Departamento
-          </MenuItem>
-          <MenuItem value={3}>
-            <HouseIcon
-              sx={{
-                fontSize: "medium",
-                color: theme.palette.primary.main,
-                paddingRight: "4px",
-              }}
-            />
-            Casa
-          </MenuItem>
-          <MenuItem value={4}>Todos</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="Ubicacion">Ubicación</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="Ubicacion"
-          /*     value={}
+					>
+						<MenuItem value={1}>
+							<BedIcon
+								sx={{
+									fontSize: "medium",
+									color: theme.palette.primary.main,
+									paddingRight: "4px",
+								}}
+							/>
+							Habitación
+						</MenuItem>
+						<MenuItem value={2}>
+							<ApartmentIcon
+								sx={{
+									fontSize: "medium",
+									color: theme.palette.primary.main,
+									paddingRight: "4px",
+								}}
+							/>
+							Departamento
+						</MenuItem>
+						<MenuItem value={3}>
+							<HouseIcon
+								sx={{
+									fontSize: "medium",
+									color: theme.palette.primary.main,
+									paddingRight: "4px",
+								}}
+							/>
+							Casa
+						</MenuItem>
+						<MenuItem value={4}>Todos</MenuItem>
+					</Select>
+				</FormControl>
+				<FormControl fullWidth>
+					<InputLabel id="location">Ubicación</InputLabel>
+					<Select
+						labelId="location"
+						id="location"
+						/*     value={}
             label={}
             onChange={} */
-        >
-          <MenuItem value={1}>Buenos Aires</MenuItem>
-          <MenuItem value={2}>Capital</MenuItem>
-          <MenuItem value={3}>Córdoba</MenuItem>
-          <MenuItem value={4}>Santiago de Chile</MenuItem>
-          <MenuItem value={5}>México D.F.</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="Ubicacion">Universidad</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="Ubicacion"
-          /*     value={}
+					>
+						<MenuItem value={1}>Buenos Aires</MenuItem>
+						<MenuItem value={2}>Capital</MenuItem>
+						<MenuItem value={3}>Córdoba</MenuItem>
+						<MenuItem value={4}>Santiago de Chile</MenuItem>
+						<MenuItem value={5}>México D.F.</MenuItem>
+					</Select>
+				</FormControl>
+				<FormControl fullWidth>
+					<InputLabel id="university">Universidad</InputLabel>
+					<Select
+						labelId="university"
+						id="university"
+						/*     value={}
             label={}
             onChange={} */
-        >
-          <MenuItem value={1}>Buenos Aires</MenuItem>
-          <MenuItem value={2}>Capital</MenuItem>
-          <MenuItem value={3}>Córdoba</MenuItem>
-          <MenuItem value={4}>Santiago de Chile</MenuItem>
-          <MenuItem value={5}>México D.F.</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel id="Ubicacion">Estadía</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="Ubicacion"
-          /*     value={}
+					>
+						<MenuItem value={1}>Buenos Aires</MenuItem>
+						<MenuItem value={2}>Capital</MenuItem>
+						<MenuItem value={3}>Córdoba</MenuItem>
+						<MenuItem value={4}>Santiago de Chile</MenuItem>
+						<MenuItem value={5}>México D.F.</MenuItem>
+					</Select>
+				</FormControl>
+				<FormControl fullWidth>
+					<InputLabel id="timelapse">Estadía</InputLabel>
+					<Select
+						labelId="timelapse"
+						id="timelapse"
+						/*     value={}
             label={}
             onChange={} */
-        >
-          <MenuItem value={1}>Hasta 30 días</MenuItem>
-          <MenuItem value={2}>Más de 30 días</MenuItem>
-        </Select>
-      </FormControl>
-      <Button type="submit" variant="contained">
-        <SearchIcon />
-      </Button>
-      <Button type="submit" variant="contained">
-        <LocationOnIcon />
-      </Button>
-    </Box>
-  );
+					>
+						<MenuItem value={1}>Hasta 30 días</MenuItem>
+						<MenuItem value={2}>Más de 30 días</MenuItem>
+					</Select>
+				</FormControl>
+				<Box
+					component="form"
+					sx={{
+						display: "flex",
+						flex: 1,
+						gap: "8px",
+						justifyContent: "space-between",
+					}}>
+					<Button type="submit" variant="contained" sx={{ flex: 1 }}>
+						<SearchIcon sx={{ color: "#fff" }} />
+					</Button>
+					<Button type="submit" variant="contained" sx={{ flex: 1 }}>
+						<LocationOnIcon sx={{ color: "#fff" }} />
+					</Button>
+				</Box>
+			</Box>
+		</form>
+	);
 }
 export default SearchBar;
