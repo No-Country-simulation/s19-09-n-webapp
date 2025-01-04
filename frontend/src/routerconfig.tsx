91e0cad1ac753edfccc8dfa6dbd4c0f190851e70
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2025-01-03 23:42:18 -0500

    Update routerconfig.tsx

--- frontend/src/routerconfig.tsx
+++ frontend/src/routerconfig.tsx
@@ -1,15 +1,21 @@
-
 import UniversityList from "./page/UniversityList";
-import DashboardLayout from "./layouts/DashboardLayout";
 import RootLayout from "./layouts/RootLayout";
-import { PageHome, Login, Register, PropertiesPage, PropertyPage, DashboardProperties } from "./page";
+import {
+  PageHome,
+  Login,
+  Register,
+  PropertiesPage,
+  PropertyPage,
+  DashboardProperties,
+  DashboardRentals,
+} from "./page";
 
 import Dashboard from "./page/Dashboard";
-import RegisterProperty from "./page/RegisterProperty"
+import RegisterProperty from "./page/RegisterProperty";
+import { Navigate, RouteObject } from "react-router-dom";
+import DashboardLayoutAccountSidebar from "./components/ui/Sidebar";
 
-
-
-export const routes = [
+export const routes: RouteObject[] = [
   {
     path: "/",
     element: <RootLayout />,
@@ -18,19 +24,22 @@ export const routes = [
       { path: "/login", element: <Login /> },
       { path: "/register", element: <Register /> },
       { path: "/properties", element: <PropertiesPage /> },
-      { path: "/properties/:id", element: <PropertyPage/>},
+      { path: "/properties/:id", element: <PropertyPage /> },
       { path: "/universities/:province", element: <UniversityList /> },
-      { path: "/registerproperty", element: <RegisterProperty /> },
-    ],    
+    ],
   },
   {
-    path: "/dashboard" ,   
-    element: <DashboardLayout />,
+    path: "/dashboard",
+    element: <DashboardLayoutAccountSidebar />,
     children: [
-      { index: true, element: <Dashboard /> },
-      {path: "/dashboard/properties", element: <DashboardProperties/>},
-
-
-    ]
+      { index: true, element: <Navigate to={"user"} /> },
+      { path: "properties/:property?", element: <DashboardProperties /> },
+      { path: "rentals", element: <DashboardRentals /> },
+      { path: "user", element: <Dashboard /> },
+      {
+        path: "registerproperty",
+        element: <RegisterProperty />,
+      },
+    ],
   },
 ];
